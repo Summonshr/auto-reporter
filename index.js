bb15d97f24c5bb99aaab538b33d2b0f55da61b8d
Suman Shrestha <suman.shrestha@CTZNBANK.COM>
2021-03-31 11:17:22 +0545

    WIP

--- index.js
+++ index.js
@@ -6,24 +6,33 @@ function caller(dir){
   fs.readdirSync('./sqls/' + dir).map(e=>{
     fs.readFile('./sqls/' + dir + '/' + e, 'utf8', async function(err, data) {
         if (err) throw err;
-        const con = await require('./connection')()
-        const result = await con.execute(data);
-        await con.close();
+        let result = false;
+        let con = false;
+        try {
+          con = await require('./connection')();
+          result = await con.execute(data);
+          await con.close();
+        } catch (error) {
+          if(con) {
+            await con.close();
+          }
+        }
   
         var xls = json2xls(result.rows);
         let dir = e.split('.')[0].split('-')
         let filename = dir.pop()
 
-        dir = './data/'+dir.join('/')
+        dir = ('./data/'+dir.join('/')).trim()
 
         if (!fs.existsSync(dir)){
           fs.mkdirSync(dir);
         }
   
-        fs.writeFileSync(dir + '/' + filename + ".xls", xls, 'binary');
+        fs.writeFileSync(dir.trim() + '/' + filename.trim() + ".csv", xls, 'binary');
     });
   })
 }
 
-
+cron.schedule('*/1 * * * *', async() => caller('minutely'))
 cron.schedule('0 * * * *', async() => caller('hourly'))
+cron.schedule('*/15 * * * *', async () => caller('every fifteen minute'))
